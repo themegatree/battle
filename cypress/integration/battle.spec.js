9b26d9205b421ca7c2b98296c7ede56b3b5c6387
unknown <char.jon3s@gmail.com>
2021-07-27 17:13:39 +0100

    added hitpoints

--- cypress/integration/battle.spec.js
+++ cypress/integration/battle.spec.js
@@ -8,5 +8,14 @@ describe('battle game user interactions', () => {
     cy.get("#p1-display-name").should('contain','Colin')
     cy.get("#p2-display-name").should('contain','Lottie')
   });
-
+  it('shows starting hitpoints' , () => {
+    cy.visit('/battle')
+    cy.get("#p1-hp").should('contain', '100');
+    cy.get("#p2-hp").should('contain', '100');
+  })
+  it('shows confirmation of attack' , () => {
+    cy.visit('/battle')
+    cy.get('#attack').click()
+    cy.get("#p1-attack-notify").should('contain', 'player 1 attacked player 2')
+  })
 });
