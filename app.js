0d90b4a4e65ba2e1c8da464fad338b70d85f3b05
Shivam Sheth <shivam@shethmail.co.uk>
2021-07-29 15:16:21 +0100

    Started Refactor

--- app.js
+++ app.js
@@ -4,36 +4,42 @@ const port = 3000
 const Attack = require('./js functionality/attack');
 const Game = require('./src/Game');
 
+//routers
+const indexRouter = require('./routes/index.js');
+const battleRouter = require('./routes/battle.js');
+// const battleAttackRouter = require('./routes/battle-attack.js');
+
 let game = new Game();
 
 //viewengine
 app.set('viewengine','ejs');
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
+app.use('/', indexRouter);
 
-app.post('/battle',(req,res) => {
+app.use('/battle', battleRouter);
 
-  game.start(req.body.p1Name, req.body.p2Name);
-  console.log('here i am')
-  console.log(game.turnTracker)
-  console.log(game.turnTracker[0])
-  console.log(game.turnTracker[0].name)
+// app.post('/battle',(req,res) => {
 
-  res.render('battle.ejs', {
-    p1Name: game.p1.name,
-    p2Name: game.p2.name,
-    p1hp: game.p1.hp,
-    p2hp: game.p2.hp,
-    attackLog: game.attackLog,
-    turnName: game.turnTracker[0].name,
-    gameStatus: `The game is in progress: ${game.gameStatus}`
-  })
-})
+//   game.start(req.body.p1Name, req.body.p2Name);
+//   console.log('here i am')
+//   console.log(game.turnTracker)
+//   console.log(game.turnTracker[0])
+//   console.log(game.turnTracker[0].name)
+
+//   res.render('battle.ejs', {
+//     p1Name: game.p1.name,
+//     p2Name: game.p2.name,
+//     p1hp: game.p1.hp,
+//     p2hp: game.p2.hp,
+//     attackLog: game.attackLog,
+//     turnName: game.turnTracker[0].name,
+//     gameStatus: `The game is in progress: ${game.gameStatus}`
+//   })
+// })
 // <p id= turn-tracker> <%= turnName %> you must click attack now </p>
 app.post('/battle-attack',(req,res) => {
+  const game = req.app.locals.game;
   //Other res methods - res.send?
   //console.log('battle attack page')
   game.attack()
@@ -44,7 +50,6 @@ app.post('/battle-attack',(req,res) => {
   // if (game.gameStatus === false) {
   //   lose: game.lose
   // };
-
   res.render('battle.ejs', {
     p1Name: game.p1.name,
     p2Name: game.p2.name,
---
+++ routes/battle.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const Game = require('../src/Game');
+
+router.post('/',(req,res) => {
+    const game = new Game();
+    console.log(req.body.p1Name)
+    game.start(req.body.p1Name, req.body.p2Name);
+  
+    res.render('battle.ejs', {
+      p1Name: game.p1.name,
+      p2Name: game.p2.name,
+      p1hp: game.p1.hp,
+      p2hp: game.p2.hp,
+      attackLog: game.attackLog,
+      turnName: game.turnTracker[0].name,
+      gameStatus: `The game is in progress: ${game.gameStatus}`
+    })
+})
+
+module.exports = router;
